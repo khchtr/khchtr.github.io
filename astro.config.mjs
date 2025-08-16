e6d33f6a998f35b46ac6d9afebfdfde5798efb11
kchtr <58407325+khchtr@users.noreply.github.com>
2025-08-16 23:59:28 +0400

    added svelte

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,10 +6,12 @@ import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 import { SITE_URL } from "./src/consts";
 
+import svelte from "@astrojs/svelte";
+
 // https://astro.build/config
 export default defineConfig({
   site: SITE_URL,
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), svelte()],
   markdown: {
     shikiConfig: {
       themes: {
